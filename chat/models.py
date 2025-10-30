b5f13ec196acce9a0e022e12808d92b683b013cb
mina0331 <kgy4dc@virginia.edu>
2025-10-30 02:55:22 -0400

    editing the models.py for chat feature

--- chat/models.py
+++ chat/models.py
@@ -1,14 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from profiles.models import Profile
 # Create your models here.
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    pic = models.ImageField(upload_to="profile_pics/", null=True, blank=True)
-
-    def __str__(self):
-        return self.name
 
 class Friend(models.Model):
     pass
