faf088016940818211d99bf450a74f11a9480b0a
mina0331 <kgy4dc@virginia.edu>
2025-11-03 12:44:18 -0500

    created models.py for posting feature

--- posting/models.py
+++ posting/models.py
@@ -30,4 +30,9 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.event} ({self.author})"
 
