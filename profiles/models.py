cbbcd6219058ce341c30c359e861075d7a2d97fc
mina0331 <kgy4dc@virginia.edu>
2025-11-02 21:11:43 -0500

    added a profile photo field for user object

--- profiles/models.py
+++ profiles/models.py
@@ -12,6 +12,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     role = models.CharField(max_length=20, choices=Role.choices, blank=True, null=True, default=None)
     display_name = models.CharField(max_length=120, blank = True)
+    profile_pic = models.ImageField(upload_to="profile_pic", blank=True, null=True)
 
     def __str__(self):
         return f"{self.user.username} ({self.get_role_display()})"
