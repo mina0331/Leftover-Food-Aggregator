777bd5718e3c91a4a9efb1387062424ebcc22a3e
mina0331 <kgy4dc@virginia.edu>
2025-11-03 12:41:24 -0500

    created models.py for posting feature

--- posting/models.py
+++ posting/models.py
@@ -1,3 +1,33 @@
+from random import choices
+
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Cuisine(models.TextChoices):
+    KOREAN = "korean", "Korean"
+    JAPANESE = "japanese", "Japanese"
+    CHINESE = "chinese", "Chinese"
+    AMERICAN = "american", "American"
+    ITALIAN = "italian", "Italian"
+    MEXICAN = "mexican", "Mexican"
+    INDIAN = "indian", "Indian"
+    THAI = "thai", "Thai"
+    VIETNAMESE = "vietnamese", "Vietnamese"
+    HALAL = "halal", "Halal"
+    VEGAN = "vegan", "Vegan"
+    BBQ = "bbq", "BBQ"
+    SEAFOOD = "seafood", "Seafood"
+    DESSERT = "dessert", "Dessert"
+    OTHER = "other", "Other"
+
+class Post(models.Model):
+    event = models.TextField()
+    event_description = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    cuisine = models.CharField(max_length=20, choices=Cuisine.choices, default = Cuisine.OTHER)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
