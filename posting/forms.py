455d839cc261c3b68ed92abba99154422cee6430
mina0331 <kgy4dc@virginia.edu>
2025-11-04 04:41:07 -0500

    creating cusine model

--- posting/forms.py
+++ posting/forms.py
@@ -4,4 +4,5 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
+        widgets = {"cuisines": forms.CheckboxSelectMultiple()}
         exclude = ("author", "created_at", "updated_at")  # author not shown
