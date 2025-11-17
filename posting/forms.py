a7c98f7cc5b4e7c2e37b9284bf77492d758075cc
umarluqman <umarluqman04@gmail.com>
2025-11-16 20:53:13 -0500

    fixed typo and errors

--- posting/forms.py
+++ posting/forms.py
@@ -1,8 +1,19 @@
 from django import forms
 from .models import Post
+from .models import Report
+
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         widgets = {"cuisines": forms.CheckboxSelectMultiple()}
         exclude = ("author", "created_at", "updated_at")  # author not shown
+
+class ReportForm(forms.ModelForm):
+    class Meta: 
+        model = Report 
+        fields = ["reason", "description"]
+        widgets = {
+            "reason": forms.Select(attrs={"class": "form-control"}),
+            "description": forms.Textarea(attrs={"class": "form-control", "rows": 3, "placeholder": "Describe what you saw (optional)"}),
+        }
