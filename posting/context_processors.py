200b46dda8630f84b679d3819dce120ca1fc98a4
mina0331 <kgy4dc@virginia.edu>
2025-11-23 23:39:14 -0500

    updated the landing page with more visual presentations

--- posting/context_processors.py
+++ posting/context_processors.py
@@ -1,11 +1,21 @@
-
 from .models import Post
+from django.utils import timezone
+from django.db.models import Q
+
 
 def unread_posts_count(request):
     if not request.user.is_authenticated:
         return {}
 
-    # posts this user has NOT read yet
-    count = Post.objects.exclude(read_users=request.user).count()
+    count = (
+        Post.objects
+        .filter(is_deleted=False)  
+        .filter(
+            Q(pickup_deadline__isnull=True) |
+            Q(pickup_deadline__gt=timezone.now())
+        )
+        .exclude(read_users=request.user)  
+        .count()
+    )
 
     return {"unread_posts_count": count}
